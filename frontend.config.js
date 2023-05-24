2c7a3c23b7e7ddbfeac46a44d17dfb4d4525e547
Anukul Pandey <anukul.030601@gmail.com>
2023-05-24 14:30:11 +0530

    fix: signRaw data

--- frontend.config.js
+++ frontend.config.js
@@ -1,17 +1,15 @@
 export const network = {
-  id: '&NETWORK_ID',
-  name: '&NETWORK_LABEL',
+  id: 'reef-mainnet',
+  name: 'Mainnet',
   tokenSymbol: 'REEF',
   tokenDecimals: 18,
   ss58Format: 42,
   coinGeckoDenom: 'reef',
-  nodeWs: '&NODE_WS',
-  backendWs: '&GQL_WS_URI',
-  backendHttp: '&GQL_HTTP_URI',
-  verificatorApi: '&VERIFICATOR_API',
+  nodeWs: 'wss://rpc.reefscan.info/ws',
+  backendWs: 'ws://squid.subsquid.io/reef-explorer/graphql',
+  backendHttp: 'https://squid.subsquid.io/reef-explorer/graphql',
+  verificatorApi: 'https://reefscan.com/api/verificator/submit-verification',
   googleAnalytics: '',
   theme: '@/assets/scss/themes/reef.scss',
 }
-console.log('network config=', network)
-
 export const paginationOptions = [10, 20, 50, 100]
