d46792a4c4869e790494805493f5e665aaedc7e8
Anukul Pandey <anukul.030601@gmail.com>
2024-01-01 13:35:12 +0530

    fix: logic to set network

--- middleware/network.js
+++ middleware/network.js
@@ -2,7 +2,11 @@ import { network as nw, reefState } from '@reef-chain/util-lib'
 import { network } from '~/frontend.config'
 
 export default function () {
-  reefState.setSelectedNetwork(
-    nw.AVAILABLE_NETWORKS[network.name.toLowerCase()]
-  )
+  let selectedNetwork
+  reefState.selectedNetwork$.subscribe((val) => (selectedNetwork = val.name))
+  if (!selectedNetwork) {
+    reefState.setSelectedNetwork(
+      nw.AVAILABLE_NETWORKS[network.name.toLowerCase()]
+    )
+  }
 }
