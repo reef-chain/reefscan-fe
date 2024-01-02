a7ccf34b9da66a76c88ed231318c2368386c6739
Anukul Pandey <anukul.030601@gmail.com>
2024-01-02 13:36:46 +0530

    feat: account nfts using account obs

--- pages/accounts.vue
+++ pages/accounts.vue
@@ -314,7 +314,7 @@ export default {
     )
   },
   destroyed() {
-    ObsPolling.removeCallback(nw.getLatestBlockAccountUpdates$([]))
+    ObsPolling.removeCallback(this.updateData)
   },
   methods: {
     async updateData() {
