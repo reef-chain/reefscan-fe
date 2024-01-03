223ebec545f082fd138ed53322c89f33fa070caa
Anukul Pandey <anukul.030601@gmail.com>
2024-01-03 12:04:15 +0530

    feat: staking polling using account obs$

--- components/StakingRewards.vue
+++ components/StakingRewards.vue
@@ -63,14 +63,15 @@
 </template>
 
 <script>
+import { network as nw } from '@reef-chain/util-lib'
 import JsonCSV from 'vue-json-csv'
 import commonMixin from '@/mixins/commonMixin.js'
 import Loading from '@/components/Loading.vue'
 import { paginationOptions } from '@/frontend.config.js'
 import Input from '@/components/Input'
 import tableUtils from '@/mixins/tableUtils'
-import BlockTimeout from '@/utils/polling.js'
 import axiosInstance from '~/utils/axios'
+import ObsPolling from '~/utils/obsPolling'
 
 export default {
   components: {
@@ -121,10 +122,13 @@ export default {
   },
   created() {
     this.updateData()
-    BlockTimeout.addCallback(this.updateData)
+    ObsPolling.addCallback(
+      nw.getLatestBlockAccountUpdates$([this.accountId]),
+      this.updateData
+    )
   },
   destroyed() {
-    BlockTimeout.removeCallback(this.updateData)
+    ObsPolling.removeCallback(this.updateData)
   },
   methods: {
     async updateData() {
