11eb3b5d3c32b6a722cd62d58cf77e12f0a458ef
Anukul Pandey <anukul.030601@gmail.com>
2025-11-14 21:58:48 +0530

    feat: gtm script

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,28 @@ export default {
       },
     ],
     link: [{ rel: 'icon', type: 'image/png', href: '/img/favicon.png' }],
+
+    // Google Tag Manager (script)
+    script: [
+      {
+        hid: 'gtm-script',
+        innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id=GTM-T8N4MBSL'+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-T8N4MBSL');`,
+        type: 'text/javascript',
+      },
+    ],
+
+    // Google Tag Manager (noscript)
+    noscript: [
+      {
+        hid: 'gtm-noscript',
+        innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T8N4MBSL"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+      },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
