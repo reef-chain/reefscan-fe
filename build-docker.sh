10e8df5b30a39001e3675d936e9aa15041b09c66
Anukul Pandey <anukul.030601@gmail.com>
2025-11-26 14:03:47 +0530

    feat: build scritp

--- build-docker.sh
+++ build-docker.sh
@@ -10,3 +10,24 @@ docker build \
   --build-arg UPLOAD_TOKEN_API="https://api.reefscan.com/api/updateTokenIcon" \
   --build-arg SOLIDITY_SCAN_API="https://api.reefscan.com/api/solidity_scan/" \
   -t reef-frontend .
+
+
+# the above command will make the frontend.config.js ->
+# export const network = {
+#   id: 'reef-mainnet',
+#   name: 'Mainnet',
+#   tokenSymbol: 'REEF',
+#   tokenDecimals: 18,
+#   ss58Format: 42,
+#   coinGeckoDenom: 'reef',
+#   nodeWs: 'ws://rpc.reefscan.info/ws',
+#   backendWs: 'ws://rpc.reefscan.info/ws',
+#   backendHttp: 'https://squid.subsquid.io/reef-explorer/graphql',
+#   verificatorApi: 'https://reefscan.com/api/verificator/submit-verification',
+#   uploadTokenApi: 'https://api.reefscan.com/api/updateTokenIcon',
+#   solidityScanData: 'https://api.reefscan.com/api/solidity_scan/',
+#   googleAnalytics: '',
+#   theme: '@/assets/scss/themes/reef.scss',
+# }
+
+# export const paginationOptions = [10, 20, 50, 100]
