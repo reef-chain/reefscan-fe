5488c495209e8f889192accdad804420c4189b32
Anukul Pandey <anukul.030601@gmail.com>
2025-12-19 14:16:45 +0530

    feat: update formo key

--- frontend.config.js
+++ frontend.config.js
@@ -1,20 +1,18 @@
 export const network = {
-  id: '&NETWORK_ID',
-  name: '&NETWORK_LABEL',
+  id: 'reef-mainnet',
+  name: 'Mainnet',
   tokenSymbol: 'REEF',
   tokenDecimals: 18,
   ss58Format: 42,
   coinGeckoDenom: 'reef',
-  nodeWs: '&NODE_WS',
-  backendWs: '&GQL_WS_URI',
-  backendHttp: '&GQL_HTTP_URI',
-  verificatorApi: '&VERIFICATOR_API',
-  uploadTokenApi: '&UPLOAD_TOKEN_API',
+  nodeWs: 'ws://rpc.reefscan.info/ws',
+  backendWs: 'ws://rpc.reefscan.info/ws',
+  backendHttp: 'https://squid.subsquid.io/reef-explorer/graphql',
+  verificatorApi: 'https://reefscan.com/api/verificator/submit-verification',
+  uploadTokenApi: 'https://api.reefscan.com/api/updateTokenIcon',
+  solidityScanData: 'https://api.reefscan.com/api/solidity_scan/',
   googleAnalytics: '',
   theme: '@/assets/scss/themes/reef.scss',
-  solidityScanApi: '&SOLIDITY_SCAN_API',
 }
-// console.log('network config=', network)
-console.log('v2.0.2');
 
 export const paginationOptions = [10, 20, 50, 100]
